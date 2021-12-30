c9a8e19d1b40bf8226f76b37fe1b483be3efaffb
Laila Fitriana <lailafitriana@me.com>
2021-12-29 22:14:47 -0500

    Add search and title to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,20 @@
+import React from "react";
 import "./home.css";
+import Search from "./search";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="title-search-container">
+          <header>
+            <h1>Recipe Box</h1>
+          </header>
+          <Search />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/components/search.js
+++ src/components/search.js
@@ -1,1 +1,14 @@
 import "./search.css";
+
+function Search(props) {
+  return (
+    <div className="search">
+      <form>
+        <input type="text" name="search" />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+}
+
+export default Search;
